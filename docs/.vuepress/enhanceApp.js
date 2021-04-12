bc8360f2211765c5836c0e79f8461917abed78bc
chengpeiquan <chengpeiquan@chengpeiquan.com>
2021-04-12 14:47:53 +0800

    Add customer code in enhanceApp.js

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -6,8 +6,10 @@ export default ({ Vue, router }) => {
     siteIdList: [
       // 主站
       '8dca8e2532df48ea7f1b15c714588691',
-      // 本站
-      'e351e4dfe35d23a0f2a26d9be8433cf5'
+      // 百度统计-站长版
+      'e351e4dfe35d23a0f2a26d9be8433cf5',
+      // 百度统计-客户版
+      'fdf049a2d869a3b673764f3ce792675f'
     ],
     isDebug: false
   });
